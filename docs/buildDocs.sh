cb3747059da377959052e1eec336eea68423e01b
Mike Conlon <mconlon@ufl.edu>
2021-06-04 19:17:51 -0400

    update spinx-build source directory

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -52,7 +52,7 @@ for current_version in ${versions}; do
    echo "INFO: Building sites for ${current_version}"
  
    # skip this branch if it doesn't have our docs dir & sphinx config
-   if [ ! -e 'docs/conf.py' ]; then
+   if [ ! -e 'docs/source/conf.py' ]; then
       echo -e "\tINFO: Couldn't find 'docs/conf.py' (skipped)"
       continue
    fi
